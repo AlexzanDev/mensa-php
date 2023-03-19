4adba320eaa9849340053bd787877a314e61ce25
AlexzanDev <alessandro@mzit.it>
2023-03-19 21:31:57 +0100

    Aggiunta configurazione di base e assets

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+// Parametri di configurazione del database
+define('DB_HOST', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'sito_mensa');
---
+++ layout/components/footer.php
@@ -0,0 +1,12 @@
+<?php
+
+// Controlla se il file è stato richiamato direttamente
+if (!defined('ABSPATH')) {
+    die('Non puoi accedere a questo file.');
+}
+?>
+
+<script src="<?php echo caricaAssets()[2] ?>"></script>
+<script src="<?php echo caricaAssets()[3] ?>"></script>
+</body>
+</html>
